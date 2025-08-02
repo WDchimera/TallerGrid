7ee323edb1d6e7db7fa2088d05b2b48a3a7c8562
WDchimera <141082635+WDchimera@users.noreply.github.com>
2025-08-02 10:52:06 -0500

    Add files via upload

--- scritp.js
+++ scritp.js
@@ -1,9 +1,7 @@
-// Obtener el botón hamburguesa y el menú de filtros
 const menuToggle = document.querySelector('.menu-toggle');
 const filtros = document.querySelector('.filtros');
 
-// Cambiar la clase "show" para mostrar u ocultar el menú
 menuToggle.addEventListener('click', () => {
-  filtros.classList.toggle('show'); // Mostrar/ocultar el menú
-  menuToggle.classList.toggle('open'); // Animación del ícono hamburguesa
+  filtros.classList.toggle('show');
+  menuToggle.classList.toggle('open');
 });
